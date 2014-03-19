0d3a43263e7cfb019aacc5ac9df0047449dcc12f
Christoph Buehler <christoph.buehler@namics.com>
2014-03-19 13:54:54 +0100

    adding color codes for recognition (more or less)

--- ColorRecognition/src/main/java/com/namics/glassshowcase/colorrecognition/activities/MainActivity.java
+++ ColorRecognition/src/main/java/com/namics/glassshowcase/colorrecognition/activities/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends Activity {
 
     private CameraPreview preview;
     private TextView color;
+    private TextView colorName;
+    private TextView colorRect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,8 @@ public class MainActivity extends Activity {
 
         FrameLayout layout = (FrameLayout) findViewById(R.id.camera_preview);
         color = (TextView) findViewById(R.id.textView);
+        colorName = (TextView) findViewById(R.id.textView2);
+        colorRect = (TextView) findViewById(R.id.editText);
         layout.addView(preview);
 
         preview.setFrameCallback(new Callable<Void>() {
@@ -37,6 +41,24 @@ public class MainActivity extends Activity {
                     @Override
                     public void run() {
                         color.setText("color is: " + Integer.toHexString(preview.pixelColor));
+
+                        colorRect.setBackgroundColor(preview.pixelColor);
+
+                        int r = (preview.pixelColor >> 16) & 0xff;
+                        int g = (preview.pixelColor >> 8) & 0xff;
+                        int b = preview.pixelColor & 0xff;
+
+                        if(isInRange(r, 11) && isInRange(g, 17) && isInRange(b, 17)) {
+                            colorName.setText("black");
+                        } else if (isInRange(r, 114) && isInRange(g, 104) && isInRange(b, 72)){
+                            colorName.setText("white");
+                        } else if (isInRange(r, 25) && isInRange(g, 55) && isInRange(b, 97)){
+                            colorName.setText("blue");
+                        } else if (isInRange(r, 125) && isInRange(g, 24) && isInRange(b, 16)){
+                            colorName.setText("red");
+                        } else {
+                            colorName.setText("not recognized");
+                        }
                     }
                 });
                 return null;
@@ -44,6 +66,11 @@ public class MainActivity extends Activity {
         });
     }
 
+    private boolean isInRange(int is, int should){
+        int min = should - (255 / 100 * 20);
+        int max = should + (255 / 100 * 20);
+        return (is >= min && is <= max);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
