53fc62706a63cadb74d975b71678368ab18065eb
Christoph Buehler <christoph.buehler@namics.com>
2014-03-19 15:07:22 +0100

    adding capsule information with pictures

--- NespressoCapsuleInformation/src/main/java/com/namics/glassshowcase/nespressocapsuleinformation/capsuleinformation/CapsuleInformation.java
+++ NespressoCapsuleInformation/src/main/java/com/namics/glassshowcase/nespressocapsuleinformation/capsuleinformation/CapsuleInformation.java
@@ -59,21 +59,14 @@ public class CapsuleInformation {
         switch (getCapsuleColorForPixelColor(colorCode)){
             case BLACK:
                 return new Ristretto();
+            case BLUE:
+                return new VivaltoLungo();
+            case RED:
+                return new DecaffeinatoLungo();
+            case WHITE:
+                return new Vanilio();
             default:
                 return new Ristretto();
-        }
-    }
-
-    public static class Ristretto extends CapsuleInformation{
-        public Ristretto(){
-            capsuleColor = CapsuleColor.BLACK;
-            title = "Ristretto";
-            origin = "Ristretto, der vorwiegend aus südamerikanischen Arabicas, (Kolumbien und Brasilien) besteht, enthält auch einige grosse, leicht säuerliche Arabicas aus Ostafrika, und eine Spur von etwas temperamentvollerem Robusta.";
-            roasting = "Die Bohnen werden langsam und separat geröstet. Auf diese Weise erhält man ein originelles Bouquet mit säuerlichen, fruchtigen und gerösteten Noten. Die sehr feine Mahlung ergibt einen Kaffee «à l'italienne»: intensiv, mit reichem Geschmack und sehr präsentem Körper.";
-            aromaProfile = "Starke Röstnoten, abgemildert durch Schokoladenoten. Ein feiner Kontrast zwischen Stärke und Bitterkeit, Säure und Fruchtnoten.";
-            cupSizes = CUP_SIZE_RISTRETTO | CUP_SIZE_ESPRESSO;
-            intensity = 10;
-            imageResource = R.drawable.ristretto_capsule;
         }
     }
 
@@ -93,5 +86,57 @@ public class CapsuleInformation {
         if (isInRange(r, 25) && isInRange(g, 55) && isInRange(b, 97)) return CapsuleColor.BLUE;
         if (isInRange(r, 125) && isInRange(g, 24) && isInRange(b, 16)) return CapsuleColor.RED;
         return null;
+    }
+
+    public static class Ristretto extends CapsuleInformation{
+        public Ristretto(){
+            capsuleColor = CapsuleColor.BLACK;
+            title = "Ristretto";
+            origin = "Ristretto, der vorwiegend aus südamerikanischen Arabicas, (Kolumbien und Brasilien) besteht, enthält auch einige grosse, leicht säuerliche Arabicas aus Ostafrika, und eine Spur von etwas temperamentvollerem Robusta.";
+            roasting = "Die Bohnen werden langsam und separat geröstet. Auf diese Weise erhält man ein originelles Bouquet mit säuerlichen, fruchtigen und gerösteten Noten. Die sehr feine Mahlung ergibt einen Kaffee «à l'italienne»: intensiv, mit reichem Geschmack und sehr präsentem Körper.";
+            aromaProfile = "Starke Röstnoten, abgemildert durch Schokoladenoten. Ein feiner Kontrast zwischen Stärke und Bitterkeit, Säure und Fruchtnoten.";
+            cupSizes = CUP_SIZE_RISTRETTO | CUP_SIZE_ESPRESSO;
+            intensity = 10;
+            imageResource = R.drawable.ristretto_capsule;
+        }
+    }
+
+    public static class VivaltoLungo extends CapsuleInformation{
+        public VivaltoLungo(){
+            capsuleColor = CapsuleColor.BLUE;
+            title = "Vivalto Lungo";
+            origin = "Eine komplexe und kontrastreiche Mischung. Die Hochlagen-Arabicas aus Südamerika bringen eine leichte Säure ein. Der Arabica aus Äthiopien steuert seine blumige Note bei und der Sud de Minas aus Brasilien stärkt den Charakter und entwickelt die Bitterkeit.";
+            roasting = "Eine separate Röstung gestattet es, den Charakter der verschiedenen Ursprungskaffees zu verfeinern. Die Mahlung entspricht der Extraktion in der grossen Tasse.";
+            aromaProfile = "Ein vielschichtiges Temperament dank der perfekt beherrschten Verbindung verschiedener Ursprungskaffees: eine schöne Röstnote, eine leichte Holznote, eine milde Getreidenote und eine zarte Blumennote.";
+            cupSizes = CUP_SIZE_LUNGO;
+            intensity = 4;
+            imageResource = R.drawable.vivalto_lungo_capsule;
+        }
+    }
+
+    public static class Vanilio extends CapsuleInformation{
+        public Vanilio(){
+            capsuleColor = CapsuleColor.WHITE;
+            title = "Vanilio";
+            origin = "Eine ausgewogene Verbindung von natürlichem Vanillearoma mit dem cremigen Livanto Grand Cru. Diese unglaublich weiche Mischung zergeht auf der Zunge und bietet eine einzigartige Geschmackserfahrung.";
+            roasting = "Ein Teil des Kaffees wird nur schwach geröstet, um die milden Noten zu betonen. Der andere Teil wird hingegen stärker geröstet, um die Mischung zu harmonisieren.";
+            aromaProfile = "Dominierende milde Noten, die an Honig und Ahornsirup erinnern, auf einer Basisnote von maltiertem Getreide.";
+            cupSizes = CUP_SIZE_ESPRESSO;
+            intensity = 6;
+            imageResource = R.drawable.vanilio_capsule;
+        }
+    }
+
+    public static class DecaffeinatoLungo extends CapsuleInformation{
+        public DecaffeinatoLungo(){
+            capsuleColor = CapsuleColor.RED;
+            title = "Decaffeinato Lungo";
+            origin = "Die Mischung besteht aus den feinsten brasilianischen und kolumbianischen Arabicas mit einem Hauch sorgfältig ausgewählter Robustas.";
+            roasting = "Langsam und intensiv, um die Röstnoten hervorzubringen.";
+            aromaProfile = "Starke Röstnoten.";
+            cupSizes = CUP_SIZE_LUNGO;
+            intensity = 3;
+            imageResource = R.drawable.decaffeinato_lungo_capsule;
+        }
     }
 }
